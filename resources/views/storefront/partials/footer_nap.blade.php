92d665e88685b0d7f53738b161b421865eddb4de
Daystar Digital <austin@lamproslabs.com>
2026-08-01 13:26:11 -0500

    feat: Remove phone number from footer contact info

--- resources/views/storefront/partials/footer_nap.blade.php
+++ resources/views/storefront/partials/footer_nap.blade.php
@@ -1,11 +1,8 @@
 @php
     $napAddress = $tenant->napAddressLine();
-    $napPhone = $tenant->phone;
 @endphp
-@if($napAddress || $napPhone)
+@if($napAddress)
     <p class="footer-nap">
-        @if($napAddress)<span class="footer-nap-address">{{ $napAddress }}</span>@endif
-        @if($napAddress && $napPhone) &middot; @endif
-        @if($napPhone)<a href="tel:{{ preg_replace('/[^0-9+]/', '', $napPhone) }}" class="footer-link footer-nap-phone">{{ $napPhone }}</a>@endif
+        <span class="footer-nap-address">{{ $napAddress }}</span>
     </p>
 @endif
